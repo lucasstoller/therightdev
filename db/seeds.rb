b32c46ec5a3b94acd32acff21fed419f095f8b3e
Lucas Stoller <l.s.stoller@gmail.com>
2024-02-15 12:04:11 -0300

    Add article creation into seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,76 @@
-User.create!(
+admin = User.create!(
   name: 'Admin',
   email: 'admin@example.com',
   password: 'secure_password',
   password_confirmation: 'secure_password',
   admin: true
 )
+
+Article.create!(
+  title: 'The First Article',
+  content: 'This is the first article in the database. It is a test article to see if the database is working properly.',
+  user_id: admin.id
+)
+
+Article.create!(
+  title: 'The Second Article',
+  content: 
+    'This is the second article in the database. It is a test article to see if the database is working properly.'\
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse scelerisque ultricies porttitor. Donec hendrerit lacinia tincidunt. '\
+    'Maecenas maximus tristique aliquet. Morbi elementum, sapien ut tempus finibus, elit felis varius felis, id malesuada tortor felis a sapien.'\
+    'Ut vulputate at libero ac tempus. Duis magna augue, scelerisque nec justo vel, tincidunt ultricies velit. Maecenas vulputate lorem sed finibus finibus. Etiam id feugiat dolor.'\
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse scelerisque ultricies porttitor. Donec hendrerit lacinia tincidunt. '\
+    'Maecenas maximus tristique aliquet. Morbi elementum, sapien ut tempus finibus, elit felis varius felis, id malesuada tortor felis a sapien.'\
+    'Ut vulputate at libero ac tempus. Duis magna augue, scelerisque nec justo vel, tincidunt ultricies velit. Maecenas vulputate lorem sed finibus finibus. Etiam id feugiat dolor.'\
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse scelerisque ultricies porttitor. Donec hendrerit lacinia tincidunt. '\
+    'Maecenas maximus tristique aliquet. Morbi elementum, sapien ut tempus finibus, elit felis varius felis, id malesuada tortor felis a sapien.'\
+    'Ut vulputate at libero ac tempus. Duis magna augue, scelerisque nec justo vel, tincidunt ultricies velit. Maecenas vulputate lorem sed finibus finibus. Etiam id feugiat dolor.'\
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse scelerisque ultricies porttitor. Donec hendrerit lacinia tincidunt. '\
+    'Maecenas maximus tristique aliquet. Morbi elementum, sapien ut tempus finibus, elit felis varius felis, id malesuada tortor felis a sapien.'\
+    'Ut vulputate at libero ac tempus. Duis magna augue, scelerisque nec justo vel, tincidunt ultricies velit. Maecenas vulputate lorem sed finibus finibus. Etiam id feugiat dolor.'\
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse scelerisque ultricies porttitor. Donec hendrerit lacinia tincidunt. '\
+    'Maecenas maximus tristique aliquet. Morbi elementum, sapien ut tempus finibus, elit felis varius felis, id malesuada tortor felis a sapien.'\
+    'Ut vulputate at libero ac tempus. Duis magna augue, scelerisque nec justo vel, tincidunt ultricies velit. Maecenas vulputate lorem sed finibus finibus. Etiam id feugiat dolor.'\
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse scelerisque ultricies porttitor. Donec hendrerit lacinia tincidunt. '\
+    'Maecenas maximus tristique aliquet. Morbi elementum, sapien ut tempus finibus, elit felis varius felis, id malesuada tortor felis a sapien.'\
+    'Ut vulputate at libero ac tempus. Duis magna augue, scelerisque nec justo vel, tincidunt ultricies velit. Maecenas vulputate lorem sed finibus finibus. Etiam id feugiat dolor.'\
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse scelerisque ultricies porttitor. Donec hendrerit lacinia tincidunt. '\
+    'Maecenas maximus tristique aliquet. Morbi elementum, sapien ut tempus finibus, elit felis varius felis, id malesuada tortor felis a sapien.'\
+    'Ut vulputate at libero ac tempus. Duis magna augue, scelerisque nec justo vel, tincidunt ultricies velit. Maecenas vulputate lorem sed finibus finibus. Etiam id feugiat dolor.'\
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse scelerisque ultricies porttitor. Donec hendrerit lacinia tincidunt. '\
+    'Maecenas maximus tristique aliquet. Morbi elementum, sapien ut tempus finibus, elit felis varius felis, id malesuada tortor felis a sapien.'\
+    'Ut vulputate at libero ac tempus. Duis magna augue, scelerisque nec justo vel, tincidunt ultricies velit. Maecenas vulputate lorem sed finibus finibus. Etiam id feugiat dolor.'\
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse scelerisque ultricies porttitor. Donec hendrerit lacinia tincidunt. '\
+    'Maecenas maximus tristique aliquet. Morbi elementum, sapien ut tempus finibus, elit felis varius felis, id malesuada tortor felis a sapien.'\
+    'Ut vulputate at libero ac tempus. Duis magna augue, scelerisque nec justo vel, tincidunt ultricies velit. Maecenas vulputate lorem sed finibus finibus. Etiam id feugiat dolor.'\
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse scelerisque ultricies porttitor. Donec hendrerit lacinia tincidunt. '\
+    'Maecenas maximus tristique aliquet. Morbi elementum, sapien ut tempus finibus, elit felis varius felis, id malesuada tortor felis a sapien.'\
+    'Ut vulputate at libero ac tempus. Duis magna augue, scelerisque nec justo vel, tincidunt ultricies velit. Maecenas vulputate lorem sed finibus finibus. Etiam id feugiat dolor.'\
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse scelerisque ultricies porttitor. Donec hendrerit lacinia tincidunt. '\
+    'Maecenas maximus tristique aliquet. Morbi elementum, sapien ut tempus finibus, elit felis varius felis, id malesuada tortor felis a sapien.'\
+    'Ut vulputate at libero ac tempus. Duis magna augue, scelerisque nec justo vel, tincidunt ultricies velit. Maecenas vulputate lorem sed finibus finibus. Etiam id feugiat dolor.'\
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse scelerisque ultricies porttitor. Donec hendrerit lacinia tincidunt. '\
+    'Maecenas maximus tristique aliquet. Morbi elementum, sapien ut tempus finibus, elit felis varius felis, id malesuada tortor felis a sapien.'\
+    'Ut vulputate at libero ac tempus. Duis magna augue, scelerisque nec justo vel, tincidunt ultricies velit. Maecenas vulputate lorem sed finibus finibus. Etiam id feugiat dolor.'\
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse scelerisque ultricies porttitor. Donec hendrerit lacinia tincidunt. '\
+    'Maecenas maximus tristique aliquet. Morbi elementum, sapien ut tempus finibus, elit felis varius felis, id malesuada tortor felis a sapien.'\
+    'Ut vulputate at libero ac tempus. Duis magna augue, scelerisque nec justo vel, tincidunt ultricies velit. Maecenas vulputate lorem sed finibus finibus. Etiam id feugiat dolor.'\
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse scelerisque ultricies porttitor. Donec hendrerit lacinia tincidunt. '\
+    'Maecenas maximus tristique aliquet. Morbi elementum, sapien ut tempus finibus, elit felis varius felis, id malesuada tortor felis a sapien.'\
+    'Ut vulputate at libero ac tempus. Duis magna augue, scelerisque nec justo vel, tincidunt ultricies velit. Maecenas vulputate lorem sed finibus finibus. Etiam id feugiat dolor.'\
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse scelerisque ultricies porttitor. Donec hendrerit lacinia tincidunt. '\
+    'Maecenas maximus tristique aliquet. Morbi elementum, sapien ut tempus finibus, elit felis varius felis, id malesuada tortor felis a sapien.'\
+    'Ut vulputate at libero ac tempus. Duis magna augue, scelerisque nec justo vel, tincidunt ultricies velit. Maecenas vulputate lorem sed finibus finibus. Etiam id feugiat dolor.'\
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse scelerisque ultricies porttitor. Donec hendrerit lacinia tincidunt. '\
+    'Maecenas maximus tristique aliquet. Morbi elementum, sapien ut tempus finibus, elit felis varius felis, id malesuada tortor felis a sapien.'\
+    'Ut vulputate at libero ac tempus. Duis magna augue, scelerisque nec justo vel, tincidunt ultricies velit. Maecenas vulputate lorem sed finibus finibus. Etiam id feugiat dolor.'\
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse scelerisque ultricies porttitor. Donec hendrerit lacinia tincidunt. '\
+    'Maecenas maximus tristique aliquet. Morbi elementum, sapien ut tempus finibus, elit felis varius felis, id malesuada tortor felis a sapien.'\
+    'Ut vulputate at libero ac tempus. Duis magna augue, scelerisque nec justo vel, tincidunt ultricies velit. Maecenas vulputate lorem sed finibus finibus. Etiam id feugiat dolor.'
+  user_id: admin.id
+)
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
